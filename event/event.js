384fa318b538f10784632963cc358e29ef4e20db
codewithayeshaa <ayshhaa.imran@gmail.com>
2025-04-22 06:53:32 +0500

    event page modified

--- event/event.js
+++ event/event.js
@@ -3,3 +3,31 @@ function scrollSlider(direction){
     let sliderAmount = 300;
     slider.scrollBy({left: sliderAmount * direction, behaviour: "smooth"})
 }
+
+$(document).ready(function () {
+    $(".card").each(function () {
+        const $card = $(this);
+        const $icon = $card.find(".bi-chevron-right");
+        const $btn = $card.find(".lrnBtn");
+
+        $icon.on("mouseenter", function () {
+            if ($icon.find(".lrnBtn").length === 0) {
+                $btn.detach().appendTo($icon);
+            }
+        });
+
+        $icon.on("mouseleave", function () {
+            if ($icon.find(".lrnBtn").length > 0) {
+                $btn.detach().insertAfter($icon);
+            }
+        });
+    });
+});
+ 
+function scrollInto(target) {
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 100); // Smooth scroll duration in ms
+  }
+
+
