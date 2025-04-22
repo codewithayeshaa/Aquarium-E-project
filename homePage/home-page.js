bb8494c12204e2e37f2b5a4798e6c9f02e470c62
codewithayeshaa <ayshhaa.imran@gmail.com>
2025-04-23 01:25:50 +0500

    home page and event page done

--- homePage/home-page.js
+++ homePage/home-page.js
@@ -5,3 +5,22 @@ function scrollToIntro() {
         block: "start"
     });
 }
+$(document).ready(function () {
+    $(".card").each(function () {
+        const $card = $(this);
+        const $icon = $card.find(".bi-chevron-right");
+        const $btn = $card.find(".lrnBtn");
+
+        $icon.on("mouseenter", function () {
+            if ($icon.find(".lrnBtn").length === 0) {
+                $btn.detach().appendTo($icon);
+            }
+        });
+
+        $icon.on("mouseleave", function () {
+            if ($icon.find(".lrnBtn").length > 0) {
+                $btn.detach().insertAfter($icon);
+            }
+        });
+    });
+});
