81c89621e0582c0e6c1a4980d433ab57c16c75e5
codewithayeshaa <ayshhaa.imran@gmail.com>
2025-04-22 06:59:29 +0500

    eventpage updated

--- event/event.js
+++ event/event.js
@@ -1,33 +1,38 @@
-function scrollSlider(direction){
-    let slider = document.getElementById("eventSlider");
-    let sliderAmount = 300;
-    slider.scrollBy({left: sliderAmount * direction, behaviour: "smooth"})
-}
 
-$(document).ready(function () {
-    $(".card").each(function () {
-        const $card = $(this);
-        const $icon = $card.find(".bi-chevron-right");
-        const $btn = $card.find(".lrnBtn");
-
-        $icon.on("mouseenter", function () {
-            if ($icon.find(".lrnBtn").length === 0) {
-                $btn.detach().appendTo($icon);
-            }
-        });
-
-        $icon.on("mouseleave", function () {
-            if ($icon.find(".lrnBtn").length > 0) {
-                $btn.detach().insertAfter($icon);
-            }
-        });
-    });
-});
- 
+// SCROLL INTO
 function scrollInto(target) {
     $('html, body').animate({
       scrollTop: $(target).offset().top
-    }, 100); // Smooth scroll duration in ms
+    }, 100); 
   }
+  
+  // SCROLL BY
+  function scrollSlider(direction){
+      let slider = document.getElementById("eventSlider");
+      let sliderAmount = 300;
+      slider.scrollBy({left: sliderAmount * direction, behaviour: "smooth"})
+    }
+
+ // LEARN MORE EFFECT
+    $(document).ready(function () {
+        $(".card").each(function () {
+            const $card = $(this);
+            const $icon = $card.find(".bi-chevron-right");
+            const $btn = $card.find(".lrnBtn");
+            
+            $icon.on("mouseenter", function () {
+                if ($icon.find(".lrnBtn").length === 0) {
+                    $btn.detach().appendTo($icon);
+                }
+            });
+            
+            $icon.on("mouseleave", function () {
+                if ($icon.find(".lrnBtn").length > 0) {
+                    $btn.detach().insertAfter($icon);
+                }
+            });
+        });
+    });
+
 
 
